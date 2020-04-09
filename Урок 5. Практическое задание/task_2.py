f8b6596065fe329c1ab9af2a98005fd4097f2032
azureswastika <lintyrex919@gmail.com>
2020-04-10 02:12:42 +0300

    Les5

--- Урок 5. Практическое задание/task_2.py
+++ Урок 5. Практическое задание/task_2.py
@@ -11,3 +11,69 @@
 Также попробуйте решить задачу вообще без collections и применить только ваши знания по ООП
 (в частности по перегрузке методов)
 """
+
+from collections import deque
+from itertools import islice
+
+FIRST_NUM = deque(list(input('Введите первое число в шестнадцатеричной системе счисления: ')))
+SECOND_NUM = deque(list(input('Введите второе число в шестнадцатеричной системе счисления: ')))
+
+LIST_OF_NUMBERS = [str(i) for i in range(10)] + ['A', 'B', 'C', 'D', 'E', 'F']
+
+if len(FIRST_NUM) > len(SECOND_NUM):
+    FIRST_NUM, SECOND_NUM = SECOND_NUM, FIRST_NUM
+
+SECOND_NUM.reverse()
+D = deque(list())
+
+J = -1
+K = 0
+for i in SECOND_NUM:
+    one = LIST_OF_NUMBERS.index(i)
+    two = LIST_OF_NUMBERS.index(FIRST_NUM[J])
+    D.append(LIST_OF_NUMBERS[(one + two + K) % 16])
+    if(one + two) >= 15:
+        K = 1
+    else:
+        K = 0
+    J -= 1
+    if J == -len(FIRST_NUM)-1:
+        break
+
+DIFFERENT = len(SECOND_NUM) - len(FIRST_NUM)
+SECOND_NUM.reverse()
+SLICE_DIFF = list(islice(SECOND_NUM, 0, DIFFERENT))
+if DIFFERENT:
+    for i in SLICE_DIFF:
+        D.append(LIST_OF_NUMBERS[(LIST_OF_NUMBERS.index(i) + K) % 16])
+        if LIST_OF_NUMBERS.index(i) + 1 >= 15:
+            K = 1
+        else:
+            K = 0
+
+if K == 1:
+    D.append('1')
+
+D.reverse()
+print(list(D))
+
+class Numb:
+    def __init__(self, num):
+        self.number = list(num)
+
+    def __str__(self):
+        return self.number.__str__()
+
+    def __add__(self, other):
+        tmp_1 = int(''.join(self.number), 16)
+        tmp_2 = int(''.join(other.number), 16)
+        return Numb(f'{tmp_1 + tmp_2:X}')
+
+    def __mul__(self, other):
+        tmp_1 = int(''.join(self.number), 16)
+        tmp_2 = int(''.join(other.number), 16)
+        return Numb(f'{tmp_1 * tmp_2:X}')
+
+
+print(Numb('1e2') + Numb('1c4f'))
+print(Numb('1e2') * Numb('1c4f'))
