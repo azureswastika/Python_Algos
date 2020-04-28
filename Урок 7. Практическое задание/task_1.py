677da31253817fc652f658b8ea8fe232e05b34f2
azureswastika <lintyrex919@gmail.com>
2020-04-28 13:18:31 +0300

    upd

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -5,7 +5,43 @@
 виде функции. Обязательно доработайте алгоритм (сделайте его умнее).
 Идея доработки: если за проход по списку не совершается ни одной сортировки, то завершение
 Обязательно сделайте замеры времени обеих реализаций
-
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+
+import time
+from random import randint
+
+def bubble(array):
+    for i in range(len(array) - 1, 0, -1):
+        for n in range(i):
+            if array[n] < array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+    print(f'{array} - bubble simple')
+
+def bubble_m(array):
+    for i in range(len(array) - 1, 0, -1):
+        swapped = False
+        for n in range(i):
+            if array[n] < array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+                swapped = True
+        if not swapped:
+            break
+    print(f'{array} - bubble modernized')
+
+NUMS = [randint(-100, 100) for i in range(10)]
+print(f'{NUMS} - исходный список')
+
+t1 = time.process_time()
+bubble(NUMS)
+t2 = time.process_time()
+bubble_time = t2 - t1
+
+t1 = time.process_time()
+bubble_m(NUMS)
+t2 = time.process_time()
+bubble_m_time = t2 - t1
+
+print(f'bubble simple - {bubble_time} s')
+print(f'bubble modernized - {bubble_m_time} s')
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -2,9 +2,42 @@
 2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
-
 Пример:
 Введите число элементов: 5
 Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
 Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
 """
+
+from random import random
+
+def list_sort(array):
+    if len(array) < 2:
+        return array
+    middle = len(array) // 2
+    left_part = array[:middle]
+    right_part = array[middle:]
+    left_part = list_sort(left_part)
+    right_part = list_sort(right_part)
+    return list_merge(left_part, right_part)
+
+
+def list_merge(list_1, list_2):
+    result = []
+    i = 0
+    j = 0
+    while i < len(list_1) and j < len(list_2):
+        if list_1[i] <= list_2[j]:
+            result.append(list_1[i])
+            i += 1
+        else:
+            result.append(list_2[j])
+            j += 1
+    result += list_1[i:]
+    result += list_2[j:]
+    return result
+
+LIST_SIZE = int(input('Введите число элементов: '))
+NUMS = [random() * 50 for i in range(LIST_SIZE)]
+
+print(NUMS)
+print(list_sort(NUMS))
