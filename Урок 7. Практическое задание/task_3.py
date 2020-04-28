49c9a90f1f3805f5ea75366ad9da86ebac5aa25c
azureswastika <lintyrex919@gmail.com>
2020-04-28 18:11:04 +0300

    Done

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -6,3 +6,38 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
 который не рассматривался на уроках
 """
+
+from random import randint, choice
+
+
+def median(orig_list, mid, pivot_list=choice):
+    if len(orig_list) == 1:
+        return orig_list[0]
+    pivot = pivot_list(orig_list)
+    left = [itm for itm in orig_list if itm < pivot]
+    right = [itm for itm in orig_list if itm > pivot]
+    pivots = [itm for itm in orig_list if itm == pivot]
+
+    if mid < len(left):
+        # если длина списка left - больше индекса медианы,
+        # то рекурсивно вызвать функцию, подать на вход список left
+        return median(left, mid)
+    elif mid < len(left) + len(pivots):
+        # если выпала медиана, то вернуть её
+        return pivots[0]
+    else:
+        # если медиана осталась в правом списке, то необходимо скорректировать индекс,
+        # с учетом того, что часть элементов остались в левом списке и списке
+        # опорных элементов, соответственно вычесть из индекса лишние элементы
+        return median(right, mid - len(left) - len(pivots))
+
+
+NUMBER_OF_ITM = int(input('Введите число элементов: '))
+if NUMBER_OF_ITM % 2 == 1:
+    ORIG_LIST = [randint(0, 100) for _ in range(NUMBER_OF_ITM)]
+    print(ORIG_LIST)
+    print(f'Медиана данного списка: {median(ORIG_LIST, (len(ORIG_LIST) // 2))}')
+    print('Для наглядности отсортированный список:')
+    print(sorted(ORIG_LIST))
+else:
+    print('Необходимо вводить количество элементов равное 2m + 1')
--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,23 @@
 
 Итог: 6 подстрок
 """
+
+import hashlib
+
+def find_substring(string):
+    hash_list = set()
+    substring_list = []
+    for i in range(len(string) + 1):
+        for x in range(i + 1, len(string) + 1):
+            hash_list.add(hashlib.sha1(string[i:x].encode('utf-8')).hexdigest())
+            substring_list.append(string[i:x])
+    
+    hash_list.remove(hashlib.sha1(string.encode('utf-8')).hexdigest())
+    substring_list.remove(string)
+    substring_list = list(set(substring_list))
+    print(f'Количество подстрок в строке "{string}" = {len(hash_list)}')
+    print(f'Подстроки: \n' +'\n'.join(substring_list))
+
+
+STRING = input('Строка: ')
+find_substring(STRING)
